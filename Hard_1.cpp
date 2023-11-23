1fb59fb0c2783b5db78c519fa3891cfe8c0fbfb5
Lakshay-2807 <garglakshay2807@gmail.com>
2023-11-23 10:54:38 +0530

    (Questions)

--- Hard_1.cpp
+++ Hard_1.cpp
@@ -1,25 +1,19 @@
-// 1. Given two numbers a and b as interval range, the task is to find the prime numbers in between this interval.
-
 #include<iostream>
 using namespace std;
-bool isPrime(int num) {
-    if(num<=1){
-        return false;
-    }
-    for(int i=2;i*i<=num;i++){
-        if(num%i==0){
-            return false;
-        }
-    }
-    return true;
-}
 int main(){
-    int a,b;
-    cout<<"Enter the interval range (a and b) : ";
-    cin>>a>>b;
-    cout<<"Prime numbers in the interval ["<<a<<", "<<b<< "] are: ";
-    for(int i=a;i<=b;i++) {
-        if(isPrime(i)) {
+    int n1,n2;
+    cout<<"Enter the range : ";
+    cin>>n1>>n2;
+    cout<<"Prime Numbers : ";
+    for(int i=n1;i<=n2;i++){
+        bool isPrime=true;
+        for(int j=2;j*j<=i;j++) {
+            if(i%j==0){
+                isPrime=false;
+                break;
+            }
+        }
+        if(isPrime&&i>1){
             cout<<i<<" ";
         }
     }
